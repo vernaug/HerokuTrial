6bac6d7a1396b9c1e6ee4e2a3146f148b452806a
vernaug <vivek.bisht@tcs.com>
2016-08-03 16:24:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def makeWebhookResult(data,req):
         speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
                  ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
-    else if "recommend" in req.get("result").get("action") :
+    else if req.get("result").get("action") == "recommendpeople":
        
         key = req.get("result").get("parameters").get("keyword")
         speech = "No recommendation pertaining to " + key + "currently." 
